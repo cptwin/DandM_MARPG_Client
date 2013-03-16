166f5b1fcdb9758d8124b3eacdacfbc24d11cc54
Dajne Win <cptwin@lolnet.co.nz>
2013-03-16 17:16:35 +1300

    Small Update

--- DandM_MARPG_Client/src/dandm_marpg_client/Core.java
+++ DandM_MARPG_Client/src/dandm_marpg_client/Core.java
@@ -29,7 +29,7 @@ public class Core {
         String sentence;
         String modifiedSentence;
         BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
-        try (Socket clientSocket = new Socket("localhost", 6789)) {
+        try (Socket clientSocket = new Socket("127.0.0.1", 6789)) {
             DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
             BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             sentence = inFromUser.readLine();
