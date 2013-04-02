a73e2d9c8c63073bfd5b942cebf2b6c67492d59f
D Win <dwin@wintech.net.nz>
2013-04-02 13:11:59 +1300

    ?

--- DandM_MARPG_Client/src/dandm_marpg_client/Main.java
+++ DandM_MARPG_Client/src/dandm_marpg_client/Main.java
@@ -17,7 +17,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Core core = new Core(); //Creates a new Core client class that does the main guts of the processing and threading
+        //Core core = new Core(); //Creates a new Core client class that does the main guts of the processing and threading
         MainJFrame mainJFrame = new MainJFrame();
         mainJFrame.setVisible(true);
     }
